5637ded10695f92e700db1bb88b2a486964e9827
aeroyals <77095251+aeroyals@users.noreply.github.com>
2021-01-18 20:29:45 +0700

    Update apakah.js

--- plugins/apakah.js
+++ plugins/apakah.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text }) => {
   conn.reply(m.chat, `
 *Pertanyaan:* ${m.text}
-*Jawaban:* ${pickRandom(['Ya','Mungkin iya','Mungkin','Mungkin tidak','Tidak','Tidak mungkin'])}
+*Jawaban:* ${pickRandom(['Ya','Mungkin iya','Mungkin','Mungkin tidak','Tidak','Tidak mungkin','Bisa jadi','Mungkin saja','Mungkin besok','mauan..'])}
 `.trim(), m)
 }
 handler.help = ['apakah <teks>?']
