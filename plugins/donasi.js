ad9cdcbf4974d7b1be2e9f93fd1e6204343f148b
aeroyals <77095251+aeroyals@users.noreply.github.com>
2021-01-18 21:18:53 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,11 +1,28 @@
 let handler = async m => m.reply(`
-╭─「 Donasi • Pulsa 」
-│ • Indosat Ooredoo [0815-1586-0089]
-│ • Telkomsel: [0813-5730-2007]
-│ • Smartfren: [0882-3543-5804]
-│ 「 Chat OWNER 」
-│ > Ingin donasi? Wa.me/6288235435804
-╰────
+╭────────
+╭⟢ *_Donasi_* ⟣╯
+╭────────
+┝༗ *Pulsa*
+┝ ➣ 0897-8842-567
+┝༗ Ovo
+┝ ➣ 0897-8842-567
+┝༗ Dana
+┝ ➣ 0897-8842-567
+┝༗ gopay
+┝ ➣ 0897-8842-567
+╭────────
+┝༗ *Sosial*
+┝ ➣ _Instagram_
+┝❑ https://instagram.com/aewoii434?igshid=tzws538qihwe
+┝ ➣ _Telegram_
+┝❑ https://t.me/aewoii
+┝ ➣ _Github_
+┝❑ https://github.com/aeroyals/AE
+▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+█▄▄░▄▄█░██░█░▄▄▀█░▄▄▀█░█▀█░▄▄█
+███░███░▄▄░█░▀▀░█░██░█░▄▀█▄▄▀█
+███░███▄██▄█▄██▄█▄██▄█▄█▄█▄▄▄█
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
 handler.tags = ['about']
